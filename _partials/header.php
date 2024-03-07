52a443c2490fb0c4ee912fbd015d3ccba212d2af
Eduardo Martínez <eduardo@aibanez.com>
2024-03-07 12:11:33 +0100

    Retirar estadisticas y sellout del menu

--- _partials/header.php
+++ _partials/header.php
@@ -2,8 +2,8 @@
 $actual_page = basename($_SERVER['SCRIPT_NAME']); // Devuelve el nombre del archivo actual
 
 $enlaces = [
-    'stats.php' => ['Estadísticas', 'fa-regular fa-bar-chart', 3],
-    'sellout.php' => ['Sellout', 'fa-regular fa-clock', 2],
+    /* 'stats.php' => ['Estadísticas', 'fa-regular fa-bar-chart', 3], */
+    /* 'sellout.php' => ['Sellout', 'fa-regular fa-clock', 2], */
     'list.php' => ['Precios', 'fa-solid fa-table', 4],
     'products.php' => ['Productos', 'fa-solid fa-cubes', 5],
     'stock.php' => ['Stock', 'fa-solid fa-boxes', 5],
