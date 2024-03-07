41169c416dda155856977b4748218ea45db67690
Eduardo Martínez <eduardo@aibanez.com>
2024-03-07 12:25:31 +0100

    Modificar pagina de inicio tras el loggeo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('_partials/init.php');
 // Redirigir a la página correspondiente según el rol del usuario
 if ($_SESSION['role_id'] >= 4) {
     // Si el usuario logeado tiene el username "seller", mostrar página en blanco
-    header("Location: stock.php");
+    header("Location: products.php");
   } else {
-    header("Location: stats.php");
+    header("Location: products.php");
 }
